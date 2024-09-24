304d8a0afa7735b3a51bfb9dd42c0a85d6ee5f3e
DjSchweppes <daltoncain0@gmail.com>
2024-09-24 10:35:11 -0500

    Final release for functioning botgit add main.py!

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ def main():
     words = get_word_count(text)
     characters = get_character_count(text)
     char_dict = alpha_chars(characters)
-    print(char_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{words} words found in the document")
+    print()
+
+    for item in char_dict:
+        print(f"The '{item['letter']} character was found {item['num']} times")
+
+    print("--- End report ---")
 
 
 def get_book_text(path):
@@ -25,11 +33,15 @@ def get_character_count(text):
             characters[char] += 1
     return characters
 
+def sort_on(d):
+    return d["num"]
+
 def alpha_chars(dict):
     list_of_chars = []
     for k in dict:
         if k.isalpha():
             list_of_chars.append({"letter": k, "num": dict[k]})
+    list_of_chars.sort(reverse=True, key=sort_on)
     return list_of_chars
 
 
